ac63e5bebfb928d300da3408596462742660c94d
RuloVH7 <rulo_van_halen@hotmail.com>
2024-04-04 20:28:40 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ya lo había hecho con las GitHub Pages'));
+app.get('/', (req, res) => res.send('Ya lo había hecho con las GitHub Pages. Ahora solo está en la nube, lo cuál está chido, creo que la forma en la que tengo mi portafolio bien podría ser esto'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
